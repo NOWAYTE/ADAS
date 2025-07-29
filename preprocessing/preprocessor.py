ab3131db0a4b382e39a8bbc07c2ef06b9a7ab576
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2025-07-29 19:11:34 +0300

    Main class with the preprocessing techniques

---
+++ preprocessing/preprocessor.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+from .compression import JPEGCompressor, BitDepthReducer
+from .filtering import AdaptiveFilter
+from .autoencoder import DenoisingAutoencoder
+from .spatial import SmartResizer
+
+class preprocessor:
+    def __init__(self, config=None):
+        self.config = config or {
+                'jpeg_quality': 75,
+                'target_bits': 5,
+                'filter_type': 'adaptive',
+                'ae_model_path': 'models/', # WIP
+                'target_size': (224, 224),
+                'pad_mode': 'reflect'
+                }
+
+        self.compressor = JPEGCompressor(self.config['jpeq_quality'])
+
+
+        def process(self,  image):
+            compressed = self.compressor.apply(image)
+
+        def fast_process(self, image):
+            compressed = self.compressor.apply(image)
